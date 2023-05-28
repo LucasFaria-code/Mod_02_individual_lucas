523d6f79ec28644cc4de1c30fed14f4833d5ddea
LucasFaria-code <128813001+LucasFaria-code@users.noreply.github.com>
2023-05-28 17:14:22 -0300

    Edição final
    Foi adicionado comentário no código explicando sua funcionalidade.

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 ## descrição do projeto
 Esse sistema verifica a compatibilidade de um candidato com a vaga de acordo com o resultado nas etapas do processo seletivo. Para isso foi criado um teste que desenvolve uma string no seguinte formato: e _t _p _s, insirindo um valor a cada novo processo seguido pelas perguntas da entrevista, teste teórico, teste prático e avaliação de soft skills.    
 
-##  status  do projeto:  Finalizado
+##  status:  Finalizado
 
 ##  Funcionalidade e demosntração:
-O avaliador insere todos os candidatos, iniciando pelo candidato 1 onde recebe as 4 avaliacões alcançadas no processo de contratação, e quando acabar deinformar é informado ao usuario se quer proceguir ou não na opção de inserir mais candidatos. Ao final de inserir e imprimir os resultados, de acordo com uma nova filtragem será impresso os candidatos aprovados.
+O avaliador insere todos os candidatos, iniciando pelo candidato 1 onde recebe as 4 avaliacões alcançadas no processo de contratação, e quando acabar de informar as notas, é informado ao usuario se quer proceguir ou não na opção de inserir mais candidatos. Ao final de inserir e definir a nota de corte, vai imprimir os resultados, de acordo com uma nova filtragem será impresso os candidatos aprovados.
 
 
 
--- mod_2.py
+++ mod_2.py
@@ -1,8 +1,12 @@
+# aqui inicia o meu trabalho individual do modulo 2 
+
 def titulo(msg):
         print(f'{msg}')  
+
+#aqui inicia a parte lógica do meu código.
        
-def lista_candidato(candidato):
-    global result, lista_aprovados
+def listCandidato(candidato):
+    global result, listAprovados
     for i in range(len(candidato)):
         e = candidato[i][0]
         t = candidato[i][1]
@@ -12,61 +16,69 @@ def lista_candidato(candidato):
         result.append(f'{i+1}° Candidato: e{e}_t{t}_p{p}_s{s}')
         
     titulo('Lista de Candidatos:')  
-    
+#esse imprime a lista de Candidatos que deram entrada no programa.   
     for c in range(len(result)):
         print(f'{result[c][:13]:.<18}', end='')
         print(f'{result[c][13:]}')
 
-def aprovados(criterios, candidato):
-    for j in range(len(candidato)):
+# aqui se da a entrada dos critérios.
+
+def aprov(criterios, candidato):
+    for g in range(len(candidato)):
         cont = 0
         for i in range(0, 4):
-            if 0 < criterios[i] <= candidato[j][i]:
+            if 0 < criterios[i] <= candidato[g][i]:
                 cont += 1
             if cont == 4:
-                lista_aprovados.append(f'{j+1}ª Candidato e_{candidato[j][0]}_t{candidato[j][1]}_p{candidato[j][2]}_s{candidato[j][3]}')
+                listAprovados.append(f'{g+1}ª Candidato e_{candidato[g][0]}_t{candidato[g][1]}_p{candidato[g][2]}_s{candidato[g][3]}')
+
+# aqui, abaixo, mostra a lista de candidatos aprovados e reprovados.
 
     titulo('Aprovados:')
      
-    for ap in range(len(lista_aprovados)):
-        print(f'{lista_aprovados[ap][:13]:.<18}', end='')
-        print(f'{lista_aprovados[ap][13:]}')
-    if len(lista_aprovados) == 0:
+    for ap in range(len(listAprovados)):
+        print(f'{listAprovados[ap][:13]:.<18}', end='')
+        print(f'{listAprovados[ap][13:]}')
+    if len(listAprovados) == 0:
         print('Nenhum aprovado!')
 
-lista_aprovados = []
+listAprovados = []
 candidato = []
 result = []
-pos = 1
+posicao = 1
   
 titulo('Insira as notas')
    
+#aqui é o algoritimo, que recebe os valores inteiros, para cada nota atribuida aos candidatos.
+
 while True:
-    print(f'{pos}ª Candidato:')
+    print(f'{posicao}° Candidato:')
     notas = [
-            int(input('Digite a nota Entrevista: ')),
-            int(input('Digite a nota Teste Teorico: ')),
-            int(input('Digite a nota Teste Prático: ')),
-            int(input('Digite a nota Soft Skill: '))
+        int(input('Digite a nota Entrevista: ')),
+        int(input('Digite a nota Teste Teorico: ')),
+        int(input('Digite a nota Teste Prático: ')),
+        int(input('Digite a nota Soft Skill: '))
             ]
     candidato.append(notas[:])
     notas.clear
 
-    resposta = input('Continuar [S] Ou para sair [N]?: ').lower()[0]
+    resposta = input('\n--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n\nDeseja adicionar mais algum cadidato a lista? \nPara Continuar com a entrada de notas do próximo candidato, digite [s] ou [n] para prosseguir para os critérios das notas de cada candidato.\nDigite aqui: ').lower()[0]
+    print('\n--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
     if resposta == 'n':
         break
-    pos +=1
-   
+    posicao +=1
+
+# aqui entra a nota minima dos criterios, para saber se o cadidato será aprovado ou reprovado.
 titulo('Quais são os Critérios:')
    
 criterios = [
-             int(input('e_ Entrevista: ')),
-             int(input('t_ Teste Teórico: ')),
-             int(input('p_ Teste Prático: ')),
-             int(input('s_ Soft Skill: '))
-             ]
+    int(input('Qual a nota de critério para a Entrevista: ')),
+    int(input('Qual a nota de critério para o Teste Teórico: ')),
+    int(input('Qual a nota de critério para o Teste Prático: ')),
+    int(input('Qual a nota de critério para o Soft Skill: '))
+            ]
 
-lista_candidato(candidato)
-aprovados(criterios, candidato)
+listCandidato(candidato)
+aprov(criterios, candidato)
 
-
+# aqui termina o código.
